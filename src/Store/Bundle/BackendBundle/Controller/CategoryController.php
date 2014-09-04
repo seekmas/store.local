4d294ec758fd36a188669dd3c1662c00cef096d5
mot <seekmas>
2014-09-05 01:20:30 +0800

    add category covert to product

--- src/Store/Bundle/BackendBundle/Controller/CategoryController.php
+++ src/Store/Bundle/BackendBundle/Controller/CategoryController.php
@@ -61,6 +61,9 @@ class CategoryController extends Controller
 
     public function editAction( Request $request , $id)
     {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $store = $this->get('store.repo')->findOneBy(['userId' => $user->getId()]);
+
         $category = $this->get('category.repo')->find( $id);
 
         if( $category == NULL)
@@ -75,14 +78,52 @@ class CategoryController extends Controller
             $data = $form->getData();
         }
 
+        $unclassified = $this->get('basket.repo')->findBy(['categoryId' => NULL , 'storeId' => $store->getId()]);
+
+        $classified = $this->get('basket.repo')->findBy(['categoryId' => $id , 'storeId' => $store->getId()]);
+
         return $this->render('StoreBackendBundle:Category:edit/index.html.twig' ,
             [
-                'category' => $category ,
-                'form'     => $form->createView() ,
+                'category'      => $category ,
+                'form'          => $form->createView() ,
+                'unclassified'  => $unclassified ,
+                'classified'    => $classified ,
             ]
         );
-
     }
+
+    public function addProductToCategoryAction( $productId , $categoryId , $dropCategory = false)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $basket = $this->get('basket.repo')->findOneBy( ['productId' => $productId]);
+
+        if( $basket == NULL)
+        {
+            throw new EntityNotFoundException();
+        }
+
+        if( $dropCategory == true)
+        {
+            $em->persist( $basket);
+            $basket->setCategory(NULL);
+            $em->flush();
+        }
+        else
+        {
+            $category = $this->get('category.repo')->find( $categoryId);
+            if($category == NULL)
+            {
+                throw new EntityNotFoundException();
+            }
+            $basket->setCategory( $category);
+            $em->persist( $basket);
+            $em->flush();
+        }
+
+
+        return $this->redirect( $this->generateUrl('product_category_edit' , ['id' => $categoryId]) );
+    }
+
 
     protected function createNewForm( Request $request , $formType , $entity)
     {
