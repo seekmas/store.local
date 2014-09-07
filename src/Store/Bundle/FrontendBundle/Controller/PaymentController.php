14744b1e90c72ce1e60bfa2be240e5e3dfd5f336
mot <seekmas>
2014-09-07 12:57:57 +0800

    add alipay repo to project

---
+++ src/Store/Bundle/FrontendBundle/Controller/PaymentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Store\Bundle\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PaymentController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        
+    }
+}
