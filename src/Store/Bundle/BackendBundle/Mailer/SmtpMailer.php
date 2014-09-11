2dbe840d4817400797f0b8f081fb4f6cac2b8fc1
mot <seekmas>
2014-09-11 18:16:29 +0800

    add line jpg image

--- src/Store/Bundle/BackendBundle/Mailer/SmtpMailer.php
+++ src/Store/Bundle/BackendBundle/Mailer/SmtpMailer.php
@@ -52,12 +52,16 @@ class SmtpMailer
     {
         $logo = $this->get('liip_imagine.cache.manager')->getBrowserPath( $store->getLogo() , 'banner');
 
+        $request = $this->get('request');
+
+        $line = 'http://'.$request->server->get('HTTP_HOST') .'/topline.jpg';
+
         $body = <<<EOF
         <div style="background-color:#d0d0d0;background-image:url();text-align:center;padding:40px;">
 
 
             <div class="mmsgLetter" style="width:780px;margin:0 auto;padding:10px;color:#333;background-color:#fff;border:0px solid #aaa;border-radius:5px;-webkit-box-shadow:3px 3px 10px #999;-moz-box-shadow:3px 3px 10px #999;box-shadow:3px 3px 10px #999;font-family:Verdana, sans-serif; ">
-            <div class="mmsgLetterHeader" style="height: 40px;background:url(/topline.jpg) repeat-x 0 0;">
+            <div class="mmsgLetterHeader" style="height: 40px;background:url( $line  ) repeat-x 0 0;">
             </div>
                 <div class="mmsgLetterContent" style="padding:40px;font-size:20px;line-height:1.5;background:url( $logo ) no-repeat top left;">
 
@@ -82,11 +86,13 @@ EOF;
     public function format($store , $title , $body )
     {
         $logo = $this->get('liip_imagine.cache.manager')->getBrowserPath( $store->getLogo() , 'banner');
+        $request = $this->get('request');
 
+        $line = 'http://'.$request->server->get('HTTP_HOST') .'/topline.jpg';
         $body = <<<EOF
         <div style="background-color:#d0d0d0;background-image:url();text-align:center;padding:40px;">
             <div class="mmsgLetter" style="width:780px;margin:0 auto;padding:10px;color:#333;background-color:#fff;border:0px solid #aaa;border-radius:5px;-webkit-box-shadow:3px 3px 10px #999;-moz-box-shadow:3px 3px 10px #999;box-shadow:3px 3px 10px #999;font-family:Verdana, sans-serif; ">
-            <div class="mmsgLetterHeader" style="height: 40px;background:url(/topline.jpg) repeat-x 0 0;">
+            <div class="mmsgLetterHeader" style="height: 40px;background:url( $line ) repeat-x 0 0;">
             </div>
                 <div class="mmsgLetterContent" style="padding:40px;font-size:20px;line-height:1.5;background:url( $logo ) no-repeat top left;">
 
